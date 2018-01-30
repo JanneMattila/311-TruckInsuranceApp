82f24fe706f9f7f8599bf570a375a21e432c7934
Janne Mattila <janne.mattila@hotmail.fi>
2018-01-30 15:41:46 +0100

    Add SQL Server and database to the ARM template

--- deploy/deploy.ps1
+++ deploy/deploy.ps1
@@ -1,9 +1,17 @@
+# To create securestring you can use following command:
+# $password = ConvertTo-SecureString -String "password" -AsPlainText -Force
+#
+# .\deploy.ps1 -DatabaseAdminPassword $password
+#
 Param (
     [string] $ResourceGroupName = "truckapp-local-rg",
     [string] $Location = "North Europe",
     [string] $Template = "$PSScriptRoot\azuredeploy.json",
     [string] $TemplateParameters = "$PSScriptRoot\azuredeploy.parameters.json",
-    [string] $PricingTier = "B1"
+    [string] $PricingTier = "B1",
+    [string] $DatabaseName = "truckdb",
+    [string] $DatabaseAdminUser = "sqluser",
+    [Parameter(Mandatory=$true)] [securestring] $DatabaseAdminPassword
 )
 
 $ErrorActionPreference = "Stop"
@@ -17,6 +25,9 @@ if ((Get-AzureRmResourceGroup -Name $ResourceGroupName -Location $Location -Erro
 # Create additional parameters that we pass to the template deployment
 $additionalParameters = New-Object -TypeName hashtable
 $additionalParameters['truckAppPlanSkuName'] = $PricingTier
+$additionalParameters['databaseName'] = $DatabaseName
+$additionalParameters['administratorLogin'] = $DatabaseAdminUser
+$additionalParameters['administratorLoginPassword'] = $DatabaseAdminPassword
 
 $result = New-AzureRmResourceGroupDeployment `
     -ResourceGroupName $ResourceGroupName `
